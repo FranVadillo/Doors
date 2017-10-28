8d9c3221a0bdda37765db61a2477bf159fc21e0b
FranVadillo <32244264+FranVadillo@users.noreply.github.com>
2017-10-28 14:10:36 +0200

    Toda la interacción con iconos
    Ya esta hecha toda la interacción con los iconos, es decir, marcarlos, desmarcarlos, colocarlos en las puertas y moverlos entre ellas.

--- Doors/source.js
+++ Doors/source.js
@@ -1,5 +1,7 @@
 var game = new Phaser.Game(1024, 576, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
+var iconos, puertas;
+
 function preload() {
     	game.load.image('fondo', 'assets/fondo.png');
     	game.load.image('puerta0', 'assets/puerta0.png');
@@ -20,30 +22,26 @@ function create(){
 	j1 = Jugador(1);
 	j2 = Jugador(2);
 	
-	//FLECHA
-	//flecha = new Flecha(40, 90);
+	//PUERTAS
+	p0 = new Puerta(50,200,0, 0);
+	p1 = new Puerta(500,200,0, 1);
+	p2 = new Puerta(200,200,0, 2);
+	p3 = new Puerta(800,200,0, 3);
+	puertas = [p0, p1, p2, p3];
+	for(var i = 0; i < 4; i++){
+		puertas[i].getSpritePuerta().inputEnabled = true;
+		puertas[i].getSpritePuerta().events.onInputDown.add(puertas[i].colocarEmoji, puertas[i]);
+	}
 
 	//ICONOS
 	i0 = new Icono(10,10,0);
 	i1 = new Icono(100,10,1);
-	x = 40;
-	var iconos = [i0, i1];
-	/*for(var i = 0; i < 2; i++){
+	iconos = [i0, i1];
+	for(var i = 0; i < 2; i++){
 		iconos[i].getSpriteIcono().inputEnabled = true;
-		iconos[i].getSpriteIcono().events.onInputDown.add(flecha.moverFlecha, this, 0, 200);
-		x += 90;
-	}*/
+		iconos[i].getSpriteIcono().events.onInputDown.add(remarcarIcono, iconos[i], 0, iconos[i].getIndice(), iconos);
+	}
 
-	//PUERTAS
-	p0 = new Puerta(50,200,0);
-	p1 = new Puerta(500,200,0);
-	p2 = new Puerta(200,200,0);
-	p3 = new Puerta(800,200,0);
-	var puertas = [p0, p1, p2, p3];
-	/*for(var i = 0; i < 4; i++){
-		puertas[i].getSpritePuerta().inputEnabled = true;
-		puertas[i].getSpritePuerta().events.onInputDown.add(eliminarSprite, this);
-	}*/
 }
 
 function update(){
@@ -57,60 +55,98 @@ function eliminarSprite(sprite){
 
 }
 
-function Puerta (x, y, tipo){
+function Puerta (x, y, tipo, ind){
 	var spritePuerta;
+	this.marcada = false;
+	this.indice = ind;
 	this.tipo = tipo;
 	this.x = x;
 	this.y = y;
 	sprite = 0;
 	switch(sprite){
-		case 0: this.spritePuerta = game.add.sprite(x, y, 'puerta0');
+		case 0: this.spritePuerta = game.add.sprite(this.x, this.y, 'puerta0');
 			break;
 	}
-	this.spritePuerta.inputEnabled = true;
-	this.spritePuerta.events.onInputDown.add(eliminarSprite, this);
+
 	this.getSpritePuerta = function(){
 		return this.spritePuerta;
+	}
+	this.desmarcar = function(){
+		this.marcada = false;
+	}
+	this.colocarEmoji = function(){
+		for(var i = 0; i < 2; i++){
+			if(iconos[i].getMarcado() && !this.marcada){
+				iconos[i].resetIcono(iconos[i].getIndice());
+				iconos[i].moveTo(x, y);
+				if(iconos[i].getPuertaAsignada() != undefined) puertas[iconos[i].getPuertaAsignada()].desmarcar();
+				iconos[i].setPuertaAsignada(this.indice);
+				this.marcada = true;
+			}
+		}
 	}
 
 }
 
-function Icono(x,y,sprite){
+function Icono(x,y,indice){
 	var spriteIcono;
-	this.sprite = sprite;
+	var puertaAsignada;
+	this.marcado = false;
+	this.indice = indice;
 	this.x = x;
 	this.y = y;
-	switch(sprite){
-		case 0: this.spriteIcono = game.add.sprite(x, y, 'emoji1');
-			this.spriteIcono.loadTexture('emoji2');
+	switch(indice){
+		case 0: this.spriteIcono = game.add.sprite(this.x, this.y, 'emoji1');
 			break;
-		case 1: this.spriteIcono = game.add.sprite(x, y, 'emoji2');
+		case 1: this.spriteIcono = game.add.sprite(this.x, this.y, 'emoji2');
 			break;
 	}
-	/*this.spriteIcono.inputEnabled = true;
-	this.spriteIcono.events.onInputDown.add(this.remarcarIcono, this, 0);
-	this.remarcarIcono = function(){
-		
-	}*/
+	
+	this.getSpriteIcono = function(){ return this.spriteIcono; }
+	this.getIndice = function(){ return this.indice; }
+	this.getMarcado = function(){ return this.marcado; }
+	this.getPuertaAsignada = function(){ return this.puertaAsignada; }
 
+	this.setPuertaAsignada = function(ind){ this.puertaAsignada = ind; }
+
+	this.moveTo = function(px, py){
+		this.spriteIcono.x = px;
+		this.spriteIcono.y = py;
+	}
+	this.marcar = function(){
+		this.marcado = true;
+	}
+	this.resetIcono = function(indice){
+		this.marcado = false;
+		switch(indice){
+			case 0: this.spriteIcono.loadTexture('emoji1');
+				break;
+			case 1: this.spriteIcono.loadTexture('emoji2');
+				break;
+		}
+	}
+
+}
+
+function remarcarIcono(sprite, cursor, indice){
+	for(var i = 0; i < 2; i++){
+		iconos[i].resetIcono(iconos[i].getIndice());
+	}
+	switch(indice){
+		case 0: this.getSpriteIcono().loadTexture('emoji1Destacado');
+			this.marcar();
+			break;
+		case 1: this.getSpriteIcono().loadTexture('emoji2Destacado');
+			this.marcar()
+			break;
+	}
 }
 
 function Jugador(tipo){
 	this.tipo = tipo;
 	if(tipo != 0) this.vidas = 5;
 }
-/*
-function Flecha(x, y){
-	var spriteFlecha;
-	this.x = x;
-	this.y = y;
-	this.spriteFlecha = game.add.sprite(x, y, 'flecha');
 
-	this.moverFlecha = function(parametroAbsurdo, parametroAbsurdo2, pos){
-		this.spriteFlecha.x = pos
-	}
-}
-*/
 /*objeto puerta
 	tipo (3 tipos)
 	icono
